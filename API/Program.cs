9949d08adf460ebd69d413338be8ca6b282ec48e
Szymon Zalewski <zalewski.sz@gmail.com>
2024-03-31 20:31:05 +0200

    End of section 2

---
+++ API/Program.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(opt => 
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+//var logger = services.GetRequiredService<ILogger<Program>>();
+
+// logger.LogInformation("*******CONFIG START******");
+// foreach (var config in builder.Configuration.AsEnumerable())
+// {
+//     if(config.Key.Contains("Connection") || config.Key.Contains("Microsoft.AspNetCore"))
+//         logger.LogInformation($"{config.Key}: {config.Value}");
+// }
+// logger.LogInformation("*******CONFIG END******");
+
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedData(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migration");
+}
+
+app.Run();
