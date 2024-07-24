94a72299b00af843905ea15080f4d09a5ee34dde
Szymon Zalewski <zalewski.sz@gmail.com>
2024-07-24 21:05:42 +0200

    End of section 9

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -4,6 +4,7 @@ import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
 import { useEffect } from "react";
 import LoadingComponent from "../../../app/layout/LoadingComponent.tsx"
+import ActivityFilters from "./ActivityFilters.tsx";
 
 export default observer(function ActivityDashboard() {
 
@@ -23,7 +24,7 @@ export default observer(function ActivityDashboard() {
         <ActivityList/>
       </Grid.Column>
       <Grid.Column width="6">
-        <h2>activity filters</h2>
+        <ActivityFilters />
       </Grid.Column>
     </Grid>
   );
--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import 'react-calendar/dist/Calendar.css'
 import './app/layout/styles.css'
 import { StoreContext, store } from './app/stores/store.ts'
 import { RouterProvider } from 'react-router-dom'
